57546779bc2dbb754256a844634c9b20bd037874
sanjanawg <146711309+sanjanawg@users.noreply.github.com>
2023-12-23 04:23:33 +0530

    Add files via upload

--- login_se.js
+++ login_se.js
@@ -21,4 +21,30 @@ document.addEventListener('DOMContentLoaded', function () {
       return true;
     }
   });
-  
+ // login.js
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  fetch('http://localhost:3000/student/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Handle the login response here
+    alert(data.message); // You might want to redirect or perform other actions
+  })
+  .catch(error => console.error('Error:', error));
+}
--- registration.js
+++ registration.js
@@ -4,7 +4,29 @@ document.addEventListener('DOMContentLoaded', function() {
     form.addEventListener('submit', function(event) {
         event.preventDefault();
 
-        // Add your form submission logic here
-        // For example, you can validate inputs and send data to the server
+        // Fetch input values
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        // Send registration data to the server
+        fetch('http://localhost:3000/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password,
+            }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // Handle response from the server
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
     });
 });
